2e477903870aa148d7935fe2a358dbe5e2cafb49
NEESHU7409 <74292354+NEESHU7409@users.noreply.github.com>
2022-04-10 10:02:37 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", bindEvents);
+
+function bindEvents(){
+    let buttons=document.getElementsByTagName("button");
+    console.log(buttons.length);
+    for(let i=0;i<buttons.length;i++){
+        buttons[i].addEventListener("click",actionfun);
+    }
+}
+function actionfun(){
+    console.log(this.innerText);
+    if(this.innerText.length==0){
+       document.getElementById("val").innerText=this.innerText;
+    }
+    else if(this.innerText=='='){
+       let res= eval(document.getElementById('val').innerText);
+       document.getElementById('val').innerText=res;
+
+    }
+    else if(this.innerText=='CE'){
+        document.getElementById('val').innerText='';
+    }
+    else{
+        let r=document.getElementById("val").innerText;
+        document.getElementById("val").innerText=r+this.innerText;
+    }
+    
+}
+
